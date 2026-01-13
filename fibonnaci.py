f1b82dc9c714b876b2407f25b2a13afb4a80fa41
Swarup Ghosh <swarupadamasuni@gmail.com>
2026-01-13 12:18:19 +0530

    Add files via upload

---
+++ fibonnaci.py
@@ -0,0 +1,20 @@
+def fibo(n):
+
+    a=0
+    b=1
+
+    print(a)
+    print(b)
+
+
+    for i in range(n-2):
+
+        
+        sum=a+b
+        print(sum)
+        a=b
+        b=sum
+        
+
+        
+print(fibo(10))
---
+++ swap.py
@@ -0,0 +1,7 @@
+a=input("a:")
+b=input("b:")
+
+def swap(a,b):
+    return b,a
+
+print(swap(a,b))
